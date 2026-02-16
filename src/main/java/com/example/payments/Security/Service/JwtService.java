e5232f91ab0e22ac5656c824ff3092f01e90de07
Tengenisu <aryanvohraplayz@gmail.com>
2026-02-16 17:22:40 +0530

    added jwtServices

--- src/main/java/com/example/payments/Security/Service/JwtService.java
+++ src/main/java/com/example/payments/Security/Service/JwtService.java
@@ -1,5 +1,57 @@
 package com.example.payments.Security.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+import java.util.*;
+import java.util.function.Function;
+
 public class JwtService {
-    
+
+    private final String secret="kVFRzJUpC4e6mAJvUSWVJ6x3jiHQEwUt1NLDREnUkqnucZzXfN/yYRnypj32PF94cIJ8fLhRmF2iaOIHsjv3Bg==";
+
+    public String extractEmail(String token){
+        return extractClaims(token , Claims::getSubject);
+    }
+
+    public <T> T extractClaims(String token , Function<Claims , T> claimsResolver){
+        final Claims claims = getTokenClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims getTokenClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey()) // create Later
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public SecretKey getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(
+            Map<String , Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000*60*60*24)) // 1 day
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    public String returnGeneratedToken(UserDetails userDetails){
+        return generateToken(new HashMap<>() , userDetails);
+    }
+    //add is token valid // expiration in a bit
+
 }
